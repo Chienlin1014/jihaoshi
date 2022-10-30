d5a1e435e232bb1960b2f10eda525fb088967c2c
Bo-Xuan Fan <coffee0127@gmail.com>
2022-10-30 15:40:08 +0800

    REST style demo

--- web/src/main/java/com/meal/controller/MealController.java
+++ web/src/main/java/com/meal/controller/MealController.java
@@ -17,14 +17,46 @@ import com.cart.model.CartProdVO;
 import com.meal.model.MealService;
 import com.meal.model.MealVO;
 
-@WebServlet("/meal/mealController")
+@WebServlet({"/meals", "/meals/*"})
 @MultipartConfig(maxFileSize = 5 * 1024 * 1024, maxRequestSize = 5 * 5 * 1024 * 1024)
 public class MealController extends HttpServlet {
     MealService mealSV = new MealService();
 
+    private final MealService mealService = new MealService();
+
+    // REST API
+    //   HTTP Method /resources/resourceId
+    // e.g.,
+    //   GET /meals ==> 讀取 Meals，i.e., getAll
+    //   GET /meals/1 ==> 讀取 id 為 1 的 Meal, i.e., findByPrimaryKey
+    //   POST /meals ==> Create a Meal
+    //   PUT /meals ==> Update a Meal
+    //   DELETE /meals/1 ==> 刪除 id 為 1 的 meal
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        doPost(req, res);
+        var pathInfo = req.getPathInfo();
+        if (pathInfo == null || "/".equals(pathInfo)) {
+            // 沒有 pathInfo 情況，例如使用者連了 `/meals` or `/meals/`
+            List<MealVO> meals = mealService.getAll();
+            if (meals != null) {
+                req.setAttribute("lastAllMeal", meals);
+                RequestDispatcher productPage = req.getRequestDispatcher("/meal/ListMealProduct.jsp");
+                productPage.forward(req, res);
+            }
+            return;
+        }
+
+        // 有 pathInfo 情況，例如使用者連了 `/meals/123`，此時的 123 就是 mealNo
+        var mealNo = Integer.parseInt(pathInfo.substring(1));
+        MealVO meal = mealService.findByMealNo(mealNo);
+        if (meal != null) {
+            req.setAttribute("meal", meal);
+            RequestDispatcher productPage = req.getRequestDispatcher("/meal/ProductPage.jsp");
+            productPage.forward(req, res);
+        } else {
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            res.sendRedirect(req.getServletContext().getContextPath() + "/404.jsp");
+        }
     }
 
     @Override
@@ -33,25 +65,6 @@ public class MealController extends HttpServlet {
         List<CartProdVO> cartProds = (ArrayList<CartProdVO>) session.getAttribute("cartProds");
         req.setCharacterEncoding("UTF-8");
         String action = req.getParameter("action");
-        if ("listAll".equals(action)) {
-
-            List<MealVO> listMeals = mealSV.getAll();
-            if (listMeals != null) {
-                req.setAttribute("listMeals", listMeals);
-                RequestDispatcher productPage = req.getRequestDispatcher("/meal/ListMealProduct.jsp");
-                productPage.forward(req, res);
-            }
-        }
-
-        if ("findByprod".equals(action)) {
-            Integer mealNo = Integer.valueOf(req.getParameter("mealNo"));
-            MealVO meal = mealSV.findByMealNo(mealNo);
-            if (meal != null) {
-                req.setAttribute("meal", meal);
-                RequestDispatcher productPage = req.getRequestDispatcher("/meal/ProductPage.jsp");
-                productPage.forward(req, res);
-            }
-        }
         if ("cartAdd".equals(action)) {
 
             Integer mealNo = Integer.valueOf(req.getParameter("mealNo"));
@@ -72,7 +85,7 @@ public class MealController extends HttpServlet {
                 session.setAttribute("cartProds", cartProds);
             } else { // 購物車內有東西時
                 for (int i = 0; i < cartProds.size(); i++) { // 尋找是否有同編號同份量商品
-                     prod = cartProds.get(i);
+                    prod = cartProds.get(i);
                     if (prod.getMeal().getMealNo() == mealNo && prod.getQuantity().equals(quantity)) {
                         prod.setAmount(prod.getAmount() + amount); // 找到就改變數量跟價格
                         prod.setPrice((int) (meal.getMealPrice() * quantity * amount));
