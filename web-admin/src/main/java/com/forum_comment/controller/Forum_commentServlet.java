43be0410aec309bcb4acea7cb1a43827d8821bcc
Tony Lu <tony81108@gmail.com>
2022-11-05 22:37:30 +0800

    no message

---
+++ web-admin/src/main/java/com/forum_comment/controller/Forum_commentServlet.java
@@ -0,0 +1,10 @@
+package com.forum_comment.controller;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/Forum_commentServlet")
+//@MultipartConfig(fileSizeThreshold = 0, maxFileSize = 5)
+
+public class Forum_commentServlet {
+
+}
