28f6e8e6b3c00cc0cac0fed769e2a10945fa0a52
Chienlin1014 <shzerokimo@gmail.com>
2022-11-15 05:07:55 +0800

    新增菜單後可寫入營養特色明細

--- web-admin/src/main/java/com/nutrient/controller/NutrientFeatureController.java
+++ web-admin/src/main/java/com/nutrient/controller/NutrientFeatureController.java
@@ -1,33 +1,49 @@
 package com.nutrient.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.nutrientFeature.model.NutrientFeatureDAO;
+import com.nutrientFeature.model.NutrientFeatureService;
 import com.nutrientFeature.model.NutrientFeatureVO;
 
 @Controller
-@RequestMapping("/nutrient/nutrientFeatureController")
+@RequestMapping("/nutrient")
 public class NutrientFeatureController {
-    private NutrientFeatureDAO dao;
+    private NutrientFeatureService nutrientFeatureSV;
 
-    public NutrientFeatureController(NutrientFeatureDAO dao) {
-        this.dao = dao;
+    public NutrientFeatureController(NutrientFeatureService nutrientFeatureSV) {
+        this.nutrientFeatureSV = nutrientFeatureSV;
     }
 
-    @PostMapping
-    public String insert() {
-        NutrientFeatureVO nutrientFeatureVO;
+    @PostMapping("/insert")
+    public String insert(@Valid @ModelAttribute("nutrientFeature") NutrientFeatureVO nutrientFeature, BindingResult result) {
 
-        return "helloworld";
+//        model.addAllAttributes("result")
+        if (result.hasErrors()) {
+            return "nutrient/NutrientFeatureInsert";
+        }
+        nutrientFeatureSV.save(nutrientFeature);
+        return "";
     }
-    @GetMapping
-    public String save() {
 
-        System.out.println(dao.findById(1).get());
+    @PostMapping("/detailInsert")
+    public String listAll(Model model) {
+        List<NutrientFeatureVO> nutrientFeatures;
+        nutrientFeatures=nutrientFeatureSV.listFeature();
+        model.addAttribute("nutrientFeatures", nutrientFeatures);
+        model.addAttribute("featureNos", new ArrayList<Integer>());
+        return "nutrient/DetailInsert";
 
-        return "helloworld";
     }
 }
