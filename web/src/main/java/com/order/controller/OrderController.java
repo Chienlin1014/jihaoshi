b6afb74b35d886d33b6dde66b9f8d58bb9db784c
Laura <lamilami1205@gmail.com>
2022-11-21 01:21:45 +0800

    線上課程結帳

--- web/src/main/java/com/order/controller/OrderController.java
+++ web/src/main/java/com/order/controller/OrderController.java
@@ -49,7 +49,7 @@ public class OrderController extends HttpServlet {
         }
 
         if ("orderList".equals(action)) {
-//            Integer memberNo = req.getParameter("memberNo");
+//          Integer memberNo = req.getParameter("memberNo");
 
             Integer memberNo = 1;
             List<OrderVO> orders = orderSV.listOrsers(memberNo);
