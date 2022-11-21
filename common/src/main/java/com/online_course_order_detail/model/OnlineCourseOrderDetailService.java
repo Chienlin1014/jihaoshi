eb43cc7e01ca94450413073bf93bc91d96c77f35
Chienlin1014 <110914314+Chienlin1014@users.noreply.github.com>
2022-11-21 14:21:40 +0800

    Update OnlineCourseOrderDetailService.java

--- common/src/main/java/com/online_course_order_detail/model/OnlineCourseOrderDetailService.java
+++ common/src/main/java/com/online_course_order_detail/model/OnlineCourseOrderDetailService.java
@@ -8,26 +8,26 @@ import com.cart.model.CartCourseVO;
 public class OnlineCourseOrderDetailService {
     private OnlineCourseOrderDetailDAO_interface dao;
 
-    public OnlineCourseOrderDetailService() {
-        dao = new OnlineCourseOrderDetailJDBCDAO();
-    }
+//    public OnlineCourseOrderDetailService() {
+//        dao = new OnlineCourseOrderDetailJDBCDAO();
+//    }
 
     public void save(String orderNo, CartCourseVO prod, Connection conn) {
         dao.insert(orderNo, prod, conn);
     }
 
-    public void updateAuthorityDetail(OnlineCourseOrderDetailVO onlineCourseOrderDetailVO) {
-        dao.update(onlineCourseOrderDetailVO);
-
-    }
-
-    public void deleteAuthorityDetail(OnlineCourseOrderDetailVO onlineCourseOrderDetailVO) {
-        dao.delete(onlineCourseOrderDetailVO);
-    }
-
-    public OnlineCourseOrderDetailVO getoneOnlineCourseOrderDetail(Integer orderNo, Integer courseNo) {
-        return dao.findByPrimaryKey(orderNo, courseNo);
-    }
+//    public void updateAuthorityDetail(OnlineCourseOrderDetailVO onlineCourseOrderDetailVO) {
+//        dao.update(onlineCourseOrderDetailVO);
+//
+//    }
+//
+//    public void deleteAuthorityDetail(OnlineCourseOrderDetailVO onlineCourseOrderDetailVO) {
+//        dao.delete(onlineCourseOrderDetailVO);
+//    }
+//
+//    public OnlineCourseOrderDetailVO getoneOnlineCourseOrderDetail(Integer orderNo, Integer courseNo) {
+//        return dao.findByPrimaryKey(orderNo, courseNo);
+//    }
 
     public List<OnlineCourseOrderDetailVO> getAll() {
         return dao.getAll();
